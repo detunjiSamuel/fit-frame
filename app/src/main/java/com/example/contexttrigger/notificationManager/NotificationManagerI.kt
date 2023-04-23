2ad7c1c8f3387b683fae76fbe15e4a0643d2e324
Adetunji Samuel <samadetunji01@gmail.com>
2023-04-23 08:23:03 +0100

    handle repeating messages

--- app/src/main/java/com/example/contexttrigger/notificationManager/NotificationManagerI.kt
+++ app/src/main/java/com/example/contexttrigger/notificationManager/NotificationManagerI.kt
@@ -33,7 +33,8 @@ class NotificationManagerI : Notification() {
 
                 var messageReceived = trigger.getNotificationTitle()
 
-                if (messageReceived != NOT_IMPLEMENTED_TRIGGER_NOTIFICATION_MESSAGE)
+                if ( messageReceived != NOT_IMPLEMENTED_TRIGGER_NOTIFICATION_MESSAGE
+                    && sameMessageNotSentRecently(messageReceived))
 
                 {
                     fireEvent(context , 1001,
@@ -42,9 +43,16 @@ class NotificationManagerI : Notification() {
                         trigger.getNotificationMessage(context)
                     )
 
+                    registerNotificationMessage(messageReceived)
+
+                    registerSentNotification()
+
                     continue
                 }
+
                 fireEvent(context , 1001 ,trigger.getCustomNotification(context))
+
+                registerSentNotification()
 
             }
         }
@@ -63,11 +71,40 @@ class NotificationManagerI : Notification() {
     private fun didUserAllowNotification(): Boolean {
 
         val sharedPref = _context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
-
-
         return sharedPref.getBoolean(USER_ALLOWED_NOTIFICATION_KEY, true)
 
     }
+
+
+    private fun sameMessageNotSentRecently(message : String):Boolean{
+
+        val sharedPref = _context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+
+        val lastTimestamp = sharedPref.getString("sent:$message", "NOT_EXIST")
+
+        if (lastTimestamp == "NOT_EXIST")
+            return true
+
+        val minutesDifference = TimeHelper()
+            .calculateMinutesDifference(lastTimestamp.toString())
+
+
+        return minutesDifference > 45
+    }
+
+    private fun registerNotificationMessage(message: String){
+
+        val sharedPref = _context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+
+        val currentTimestamp = System.currentTimeMillis().toString()
+
+        with(sharedPref.edit()) {
+            putString("sent:$message", currentTimestamp)
+            apply()
+        }
+
+    }
+
 
     private fun isNotNightTime(): Boolean {
         return !TimeHelper().isNightTime()
@@ -85,6 +122,16 @@ class NotificationManagerI : Notification() {
             .calculateMinutesDifference(lastTriggerNotificationTimestamp.toString())
 
         return minutesDifference > 15
+
+    }
+
+    private fun registerSentNotification() {
+
+        val sharedPref = _context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
+        with(sharedPref.edit()) {
+            putString("lastTriggerNotification",System.currentTimeMillis().toString())
+            apply()
+        }
 
     }
 
