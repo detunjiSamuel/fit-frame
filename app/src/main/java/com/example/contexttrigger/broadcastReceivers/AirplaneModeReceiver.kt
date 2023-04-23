b99cbff15a0a4478077a98976c5ead68cd282a1c
Adetunji Samuel <samadetunji01@gmail.com>
2023-04-23 04:35:49 +0100

    add resource check

--- app/src/main/java/com/example/contexttrigger/broadcastReceivers/AirplaneModeReceiver.kt
+++ app/src/main/java/com/example/contexttrigger/broadcastReceivers/AirplaneModeReceiver.kt
@@ -3,21 +3,42 @@ package com.example.contexttrigger.broadcastReceivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.example.contexttrigger.components.sensorManager.SensorController
+import com.example.contexttrigger.dataProducers.LOCATION_RECORDING_PUBLIC_NAME
 
 class AirplaneModeReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_AIRPLANE_MODE_CHANGED) {
             val isAirplaneModeOn = intent.getBooleanExtra("state", false)
+
+            val resourceName = "airplane-mode"
+
             if (isAirplaneModeOn) {
                 // Airplane mode is on
-                val intent = Intent("com.example.contexttrigger.AIRPLANE_MODE_ON")
-                context.sendBroadcast(intent)
+
+
+                val intent = Intent(context , SensorController::class.java)
+
+                intent.putExtra("IS_REPORTING" , "DEVICE_RESOURCE_CHANGE" )
+
+                intent.putExtra("DATA" , "$resourceName|NO")
+
+                context.startService(intent)
+
+
             } else {
                 // Airplane mode is off
-                val intent = Intent("com.example.contexttrigger.AIRPLANE_MODE_OFF")
-                context.sendBroadcast(intent)
+                val intent = Intent(context , SensorController::class.java)
+
+                intent.putExtra("IS_REPORTING" , "DEVICE_RESOURCE_CHANGE" )
+
+                intent.putExtra("DATA" , "$resourceName|YES")
+
+                context.startService(intent)
             }
         }
     }
+
+
 }
--- app/src/main/java/com/example/contexttrigger/broadcastReceivers/InternetConnectivityReceiver.kt
+++ app/src/main/java/com/example/contexttrigger/broadcastReceivers/InternetConnectivityReceiver.kt
@@ -3,17 +3,38 @@ package com.example.contexttrigger.broadcastReceivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.example.contexttrigger.components.sensorManager.SensorController
 
 class InternetConnectivityReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
 
 
+        val resourceName = "airplane-mode"
+
+
         val hasInternet = intent.getStringExtra("DATA")
+
         if (hasInternet == "YES") {
-            // Do something when internet is connected
+
+
+            val intent = Intent(context , SensorController::class.java)
+
+            intent.putExtra("IS_REPORTING" , "DEVICE_RESOURCE_CHANGE" )
+
+            intent.putExtra("DATA" , "$resourceName|YES")
+
+            context.startService(intent)
+
         } else  {
-            // Do something when internet is disconnected
+
+            val intent = Intent(context , SensorController::class.java)
+
+            intent.putExtra("IS_REPORTING" , "DEVICE_RESOURCE_CHANGE" )
+
+            intent.putExtra("DATA" , "$resourceName|NO")
+
+            context.startService(intent)
         }
     }
 }
